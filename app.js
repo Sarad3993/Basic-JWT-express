01944140c526d63bca66de22d83caf212254a974
Sarad3993 <risingstar3993@gmail.com>
2023-01-30 19:36:52 +0545

    set up controller and routes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 
 
-
+const mainRouter = require('./routes/main')
 const notFoundMiddleware = require('./middlewares/not-found')
 const errorHandlerMiddleware = require('./middlewares/error-handler')
 
@@ -13,6 +13,7 @@ const errorHandlerMiddleware = require('./middlewares/error-handler')
 app.use(express.static('./public'))
 app.use(express.json())
 
+app.use('/api/v1',mainRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
---
+++ controllers/main.js
@@ -0,0 +1,15 @@
+
+const login = async (req, res) => {
+  res.send("Fake login/register");
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+      msg: `Hello, John Doe`,
+      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
+};
+
+
+module.exports = { login, dashboard }
